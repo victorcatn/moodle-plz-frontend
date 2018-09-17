82e619a3e29f5310ad761af85fd07c73ce09757c
Victor Daniel Cataño Calderon <vdcatanoc@unal.edu.co>
2018-09-16 20:47:18 -0500

    MP-13 improved member selection in MP-03 group creation

--- src/app/group/service/group-service.service.ts
+++ src/app/group/service/group-service.service.ts
@@ -14,7 +14,7 @@ const httpOptions = {
 })
 export class GroupService {
 
-  private groupUrl = 'http://localhost:8080/group';  // URL to web api
+  private groupUrl = 'http://localhost:8080/groups';  // URL to web api
 
   constructor(
     private http: HttpClient) { }
--- src/app/staffmember/service/staffmember.service.ts
+++ src/app/staffmember/service/staffmember.service.ts
@@ -14,7 +14,7 @@ const httpOptions = {
 })
 export class StaffMemberService {
 
-  private staffMembersUrl = 'http://localhost:8080/staffmember';  // URL to web api
+  private staffMembersUrl = 'http://localhost:8080/staffmembers';  // URL to web api
 
   constructor(
     private http: HttpClient) { }
